6cf17c242e412f1b59c324b77c07c2c8eddb3de1
ghofrane67 <mahdighofrane01@gmail.com>
2023-12-04 08:36:12 +0100

    first version

---
+++ src/main/java/com/example/Nessa/NessaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Nessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NessaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Nessa/NessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Nessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NessaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
